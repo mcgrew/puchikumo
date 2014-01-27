2da32a1aa770f38f0b213cea053559bf695515d5
Thomas McGrew <tjmcgrew@gmail.com>
2014-01-27 12:58:26 -0700

    More work on dropbox-style functionality

--- uploadserver.py
+++ uploadserver.py
@@ -28,100 +28,12 @@ from glob import glob
 from time import ctime
 from email.utils import formatdate
 import math
+from urllib import unquote_plus as unquote
 
 
 VERSION = "0.3.0-pre"
 UPLOAD_BUTTON = "uploadButton"
 UPLOAD_FILE = "upload"
-
-PROGRESS_HTML = """<!DOCTYPE html>
-<html>
-  <head>
-    <title>Upload Progress</title>
-    <style type="text/css">
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: Arial, Helvetica, sans-serif;
-      }
-      #progress {
-        width: 400px;
-        height: 1.2em;
-        border: 1px solid black;
-        position: relative;
-        text-align: center;
-        overflow: hidden;
-      }
-      #progressbar {
-        width: 0;
-        height: 100%%;
-        background-color: #66ff66;
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: -1;
-      }
-      #fileBox {
-        padding: 1em;
-      }
-      #progressIndicator {
-        font-weight: bold;
-      }
-      .complete {
-        font-size: 0.8em;
-        color: #33aa33;
-      }
-      .inTransit {
-        font-size: 0.8em;
-        color: #dd4444;
-      }
-    </style>
-  </head>
-  <body>
-
-<div id='progress'>
-  <div id='progressbar'>
-  </div>
-  <span id='progressIndicator'>0%%</span>
-</div>
-<div id='fileBox'>
-  <h3>Files:</h3>
-  <span id='fileList'>
-  </span>
-</div>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js">
-</script>
-<script><!--
-  function updateProgress( ) {
-    jQuery.getJSON( '%s', function( data ) {
-      if ( data.read ) {
-        if ( data.read == data.total ) {
-          setTimeout( close, 3000 );
-        }
-        progressbar = jQuery( '#progressbar' );
-        progressIndicator = jQuery( '#progressIndicator' );
-        fileList = jQuery( '#fileList' );
-        progressbar.css( 'width', data.read / data.total * 400 );
-        progressIndicator.html( 
-          Math.round( data.read / data.total * 100 ) + "%%")
-        filesHTML = data.files.join( 
-          ' <span class="complete">Complete!</span><br />');
-        if ( data.files.length )
-          filesHTML += ' <span class="complete">Complete!</span><br />';
-        if ( data.current )
-          filesHTML += 
-            data.current + ' <span class="inTransit">Transferring...</span>';
-        fileList.html( filesHTML );
-      }
-    });
-  }
-  $( function( ) {
-    setInterval( updateProgress, 2000 );
-  });
-  --></script>
-  </body>
-</html>
-"""
 
 class ForkingServer(ForkingMixIn, HTTPServer):
   pass
@@ -132,9 +44,32 @@ class UploadHandler(BaseHTTPRequestHandler):
 
   def do_HEAD( self ):
     if OPTIONS.download:
-      if self.path.startswith( '/files/' ):
-        self._file_request( self.path[6:], True )
-        return
+      self._file_request( self.path[1:], True )
+      return
+
+  def _start_session(self):
+    if not self.cookies.has_key(OPTIONS.sessionkey):
+      self._set_cookie(OPTIONS.sessionkey,
+        ''.join( random.choice( string.ascii_uppercase + string.digits ) 
+        for i in range(32)))
+
+  def _set_cookie( self, name, value, path='/' ):
+    self.cookies[name] = value
+    self.send_header( 'Set-Cookie', '%s=%s; Path=%s' % ( name, value, path ))
+
+  def _parse_cookies( self ):
+    """
+    Reads the cookie information sent from the client and places it in 
+    the self.cookies dict. If no cookie information has been sent, this dict 
+    will be empty (length of 0).
+    """
+    # parse the cookies
+    if self.headers.has_key( 'Cookie' ):
+      cookie_pieces = re.split( "(.*?)=(.*?)(:?; |$)", self.headers['Cookie'] )
+      self.cookies = dict( zip( cookie_pieces[1::4], cookie_pieces[2::4]))
+    else:
+      self.cookies = dict( )
+    return self.cookies
 
   def do_GET( self ):
     """
@@ -146,19 +81,11 @@ class UploadHandler(BaseHTTPRequestHandler):
     self.progress_url = OPTIONS.url
     if self.progress_url.endswith( '/' ):
       self.progress_url = self.progress_url[ :-1 ]
-    self.progress_url += '/progress'
+    self.progress_url += '/_progress'
     if OPTIONS.progress:
-      if self.path == "/progress": 
+      if self.path == "/_progress": 
         # send the progres JSON feed
         self._progress( )
-        return
-      if self.path == "/progress.html":
-        # send the progress page HTML
-        self._get_progress_page( )
-        return
-    if OPTIONS.download:
-      if self.path.startswith( '/files/' ):
-        self._file_request( self.path[6:] )
         return
     self._send_get_response( )
 
@@ -182,16 +109,154 @@ class UploadHandler(BaseHTTPRequestHandler):
     self.wfile.write( progressfile.read( ))
     progressfile.close( )
     self.wfile.close( )
-    
 
-  def _get_progress_page( self ):
-    reply = PROGRESS_HTML % self.progress_url
+  def _preprocess_get( self ):
+    """
+    GET request pre-processing. By default this method does nothing, override
+    it to perform some tasks before processing of the request.
+    """
+    pass
+
+  def _send_get_response( self ):
+    if OPTIONS.download:
+      self._file_request( self.path[1:] )
+    else:
+      self.send_response( 200 )
+      self.send_header( 'Content-Type', 'text/html' )
+      if OPTIONS.progress:
+        self._start_session( )
+      self.end_headers( )
+      self.wfile.write("<!DOCTYPE html>\n<html><head></head><body>")
+      self._upload_form()
+      self.wfile.write( "</body></html>" )
+
+  def _upload_form(self):
+    self.wfile.write( """
+          <form method="post" name="fileUpload" action="%s" 
+          enctype="multipart/form-data">
+          <input type="file" name="%s" multiple="true"/>
+            <button id='upload' type="submit" name="%s" value="true">Upload file(s)</button>
+          </form>
+      """ % (OPTIONS.url, self.upload_file, self.upload_button))
+
+  def _file_request( self, path='/', head_only=False ):
+    # user is requesting a file, send it.
+    real_path = unquote(OPTIONS.upload_folder + '/' + path)
+    if not os.path.exists( real_path ):
+      self.send_error( 404 )
+      if OPTIONS.progress:
+        self._start_session( )
+      return
+        
+    if os.path.isfile( real_path ):
+      self._send_file(real_path, head_only)
+    if os.path.isdir( real_path ):
+      self._directory_listing(real_path, head_only)
+
+  def _send_file(self, path, head_only=False):
     self.send_response( 200 )
-    self.send_header( "Content-Type", 'text/html' )
-    self.send_header( "Content-Length", len( reply ))
+    mimetype = mimetypes.guess_type( path )[0]
+    stats = os.stat( path )
+    if not mimetype:
+      mimetype = 'text/plain'
+    self.send_header( 'Content-Type', mimetype )
+    self.send_header( 'Content-Length', stats.st_size )
+    self.send_header( 'Last-Modified', formatdate( stats.st_mtime, usegmt=True ))
+    if OPTIONS.progress:
+      self._start_session( )
     self.end_headers( )
-    self.wfile.write( reply )
+    if not head_only:
+      requested_file = open( path, 'r' )
+      self.wfile.write( requested_file.read( ))
+      requested_file.close( )
     self.wfile.close( )
+    return
+
+  def _directory_listing(self, path, head_only=False):
+    self.send_response( 200 )
+    self.send_header('Content-Type', 'text/html')
+    self.end_headers()
+    # user requested a directory, list the files in it.
+    file_list = glob( path + '/*' )
+    self.end_headers( )
+    if not head_only:
+      self.wfile.write( "<!DOCTYPE html><html><head></head><body>" )
+      relpath = os.path.relpath(path, OPTIONS.upload_folder)
+      relpath = '/' if relpath == '.'else '/'+relpath 
+      self.wfile.write("<h1>Directory listing for %s</h1><br>" % relpath)
+      self.wfile.write( "<table cellpadding='3'><tbody>" )
+      self.wfile.write( 
+        "<tr><th>Name</th><th>Size</th><th>Type</th><th>Last Modified</th></tr>" )
+      if path != '/':
+        self.wfile.write( 
+          "<tr><td><a href='../'>Parent Directory</td><td colspan=3></td></tr>" )
+      # file size multipliers
+      multipliers = ( '%dB', '%dKB', '%dMB', '%dGB', '%sTB' )
+      for f in file_list:
+        relpath = os.path.relpath( f, path )
+        filetype = mimetypes.guess_type(path + f)[0]
+        if not filetype:
+          filetype = '-'
+        stats = os.stat( f )
+        file_size = self._get_file_size( f )
+        # magnitude of the file size
+        size_mag = int(math.log( file_size, 1024 )) if file_size else 0
+        if os.path.isdir( f ):
+          relpath += '/'
+          filetype = 'directory'
+        self.wfile.write( 
+          "<tr><td><a href='%s'>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>" % 
+          ('/' + relpath, relpath, 
+            multipliers[ size_mag ] % ( file_size >> ( 10 * size_mag )),
+            filetype, ctime( stats.st_mtime ))
+        )
+      self.wfile.write( "</tbody></table>" )
+      self.wfile.write( "<br><br>" )
+      self._upload_form()
+      self.wfile.write( "</body></html>" )
+      return
+
+  def _get_file_size( self, path ):
+    if os.path.isdir( path ):
+      file_list = glob( path + '/*' )
+      dir_size = 0
+      for f in file_list:
+        dir_size += self._get_file_size( f )
+      return dir_size
+    else:
+      return os.stat( path ).st_size
+
+  def _init_progress( self ):
+    """
+    Initilizes the progress json feed.
+    """
+    progressfilename = os.sep.join([OPTIONS.tmp_folder, "progress", 
+        self.cookies[OPTIONS.sessionkey]])
+    progressfile = open( progressfilename, 'w')
+    progress = { 'files':[], 'read': 0, 'total': self.content_length }
+    progressfile.write( json.dumps( progress ))
+    progressfile.close( )
+
+  def _update_progress( self, current_transfer=None ):
+    """
+    Adds uploaded files and current upload progress to a file for the JSON 
+    progress feed.
+    """
+    # create a file with a different name and rename it. This should prevent the
+    # progress feed thread from sending an incomplete file.
+    progressfilename = os.sep.join([OPTIONS.tmp_folder, "progress", 
+        self.cookies[OPTIONS.sessionkey]])
+    progressfile = open( progressfilename + '~', 'w')
+    progress = { 'files': [os.path.basename( x ) for x in self.postdict[ 'files' ]], 
+      'read': (( self.content_length - self.remaining_content )
+        if self.remaining_content > 0 else self.content_length ),
+      'total': self.content_length }
+    if current_transfer:
+      progress['current'] = current_transfer
+    progressfile.write( json.dumps( progress ))
+    progressfile.close( )
+    os.rename( progressfilename + '~', progressfilename )
+
 
   def do_POST( self ):
     """
@@ -214,29 +279,6 @@ class UploadHandler(BaseHTTPRequestHandler):
     self._preprocess_post( )
     self._read_post_data( )
     self._send_post_response( )
-
-
-    if not self.cookies.has_key( OPTIONS.sessionkey ):
-      self._set_cookie(  OPTIONS.sessionkey,
-        ''.join( random.choice( string.ascii_uppercase + string.digits ) 
-        for i in range(32)))
-
-  def _set_cookie( self, name, value, path='/' ):
-    self.send_header( 'Set-Cookie', '%s=%s; Path=%s' % ( name, value, path ))
-
-  def _parse_cookies( self ):
-    """
-    Reads the cookie information sent from the client and places it in 
-    the self.cookies dict. If no cookie information has been sent, this dict 
-    will be empty (length of 0).
-    """
-    # parse the cookies
-    if self.headers.has_key( 'Cookie' ):
-      cookie_pieces = re.split( "(.*?)=(.*?)(:?; |$)", self.headers['Cookie'] )
-      self.cookies = dict( zip( cookie_pieces[1::4], cookie_pieces[2::4]))
-    else:
-      self.cookies = dict( )
-    return self.cookies
 
   def _read_post_data( self ):
     """
@@ -287,127 +329,12 @@ class UploadHandler(BaseHTTPRequestHandler):
     """
     pass
 
-  def _preprocess_get( self ):
-    """
-    GET request pre-processing. By default this method does nothing, override
-    it to perform some tasks before processing of the request.
-    """
-    pass
-
   def _preprocess_post( self ):
     """
     POST request pre-processing. By default this method does nothing, override
     it to perform some tasks before processing of the request.
     """
     pass
-
-  def _send_get_response( self ):
-    self.send_response( 200 )
-    if OPTIONS.progress:
-      self._start_session( )
-    self.send_header( 'Content-Type', 'text/html' )
-    self.end_headers( )
-    self.wfile.write( """<!DOCTYPE html>
-      <html>
-        <head>
-        </head>
-        <body>
-          <form method="post" name="fileUpload" action="%s" 
-          enctype="multipart/form-data">
-            <input type="file" name="%s" multiple="true"/><br />
-            <button id='upload' type="submit" name="%s" value="true">Upload</button>
-          </form>
-        """ % ( OPTIONS.url, self.upload_file, self.upload_button ))
-    if OPTIONS.progress:
-      self.wfile.write( """
-        <script 
-          src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
-        </script>
-        <script><!--
-          $(function( ) {
-            jQuery( '#upload' ).click( function( ) {
-              jQuery('*').css( 'cursor','wait');
-              if ( ! jQuery.browser.msie ) // progress bar doesn't work in IE.
-                open( '%s.html', '',
-                  'width=402,height=400,titlebar=no,toolbar=no,status=no,' +
-                  'menubar=no,location=no' );
-            });
-          });
-        --></script>""" % ( self.progress_url ))
-    self.wfile.write( "</body></html>" )
-
-  def _file_request( self, path='/', head_only=False ):
-    # user is requesting a file, send it.
-    real_path = OPTIONS.upload_folder + path 
-    if not os.path.exists( real_path ):
-      self.send_error( 404 )
-      return
-        
-    if os.path.isfile( real_path ):
-      self.send_response( 200 )
-      mimetype = mimetypes.guess_type( real_path )[0]
-      stats = os.stat( real_path )
-      if not mimetype:
-        mimetype = 'text/plain'
-      self.send_header( 'Content-Type', mimetype )
-      self.send_header( 'Content-Length', stats.st_size )
-      self.send_header( 'Last-Modified', formatdate( stats.st_mtime, usegmt=True ))
-      self.end_headers( )
-      if not head_only:
-        requested_file = open( real_path, 'r' )
-        self.wfile.write( requested_file.read( ))
-        requested_file.close( )
-      self.wfile.close( )
-      return
-
-    if os.path.isdir( real_path ):
-      # user requested a directory, list the files in it.
-      file_list = glob( real_path + '*' )
-      self.send_response( 200 )
-      self.end_headers( )
-      if not head_only:
-        self.wfile.write( "<!DOCTYPE html><html><head></head><body>" )
-        self.wfile.write( "<h1>Index of %s</h1><br>" % path )
-        self.wfile.write( "<table cellpadding='3'><tbody>" )
-        self.wfile.write( 
-          "<tr><th>Name</th><th>Size</th><th>Type</th><th>Last Modified</th></tr>" )
-        if path != '/':
-          self.wfile.write( 
-            "<tr><td><a href='../'>Parent Directory</td><td colspan=3></td></tr>" )
-        # file size multipliers
-        multipliers = ( '%dB', '%dKB', '%dMB', '%dGB', '%sTB' )
-        for f in file_list:
-          relpath = os.path.relpath( f, real_path )
-          filetype = mimetypes.guess_type(real_path + f)[0]
-          if not filetype:
-            filetype = '-'
-          stats = os.stat( f )
-          file_size = self._get_file_size( f )
-          # magnitude of the file size
-          size_mag = int(math.log( file_size, 1024 )) if file_size else 0
-          if os.path.isdir( f ):
-            relpath += '/'
-            filetype = 'directory'
-          self.wfile.write( 
-            "<tr><td><a href='%s'>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>" % 
-            ('/files' + path + relpath, relpath, 
-              multipliers[ size_mag ] % ( file_size >> ( 10 * size_mag )),
-              filetype, ctime( stats.st_mtime ))
-          )
-        self.wfile.write( "</tbody></table>" )
-        self.wfile.write( "</body></html>" )
-      self.wfile.close( )
-      return
-
-  def _get_file_size( self, real_path ):
-    if os.path.isdir( real_path ):
-      file_list = glob( real_path + '/*' )
-      dir_size = 0
-      for f in file_list:
-        dir_size += self._get_file_size( f )
-      return dir_size
-    else:
-      return os.stat( real_path ).st_size
 
   def _send_post_response( self ):
     self.send_response( 200 )
@@ -453,7 +380,7 @@ class UploadHandler(BaseHTTPRequestHandler):
     if filename:
       if not os.path.exists( self.upload_folder ):
         os.makedirs( self.upload_folder )
-      value_buffer = open( '%s/%s' % ( self.upload_folder, filename), 'wb' )
+      value_buffer = open( '%s%s/%s' % ( self.upload_folder, self.path, filename), 'wb' )
     else:
       value_buffer = StringIO( )
     prev_line = False
@@ -504,38 +431,6 @@ class UploadHandler(BaseHTTPRequestHandler):
 
   def _finished( self ):
     return self.remaining_content <= 0 and not self.readbuf
-
-  def _init_progress( self ):
-    """
-    Initilizes the progress json feed.
-    """
-    progressfilename = os.sep.join([OPTIONS.tmp_folder, "progress", 
-        self.cookies[OPTIONS.sessionkey]])
-    progressfile = open( progressfilename, 'w')
-    progress = { 'files':[], 'read': 0, 'total': self.content_length }
-    progressfile.write( json.dumps( progress ))
-    progressfile.close( )
-
-  def _update_progress( self, current_transfer=None ):
-    """
-    Adds uploaded files and current upload progress to a file for the JSON 
-    progress feed.
-    """
-    # create a file with a different name and rename it. This should prevent the
-    # progress feed thread from sending an incomplete file.
-    progressfilename = os.sep.join([OPTIONS.tmp_folder, "progress", 
-        self.cookies[OPTIONS.sessionkey]])
-    progressfile = open( progressfilename + '~', 'w')
-    progress = { 'files': [os.path.basename( x ) for x in self.postdict[ 'files' ]], 
-      'read': (( self.content_length - self.remaining_content )
-        if self.remaining_content > 0 else self.content_length ),
-      'total': self.content_length }
-    if current_transfer:
-      progress['current'] = current_transfer
-    progressfile.write( json.dumps( progress ))
-    progressfile.close( )
-    os.rename( progressfilename + '~', progressfilename )
-
 
 optParser = OptionParser(version="%%prog %s" % VERSION, usage="%prog [options]")
 optParser.add_option( "-a", "--address", dest="address", default="",
